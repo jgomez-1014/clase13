890cee81bd018f1136f533b5a26007962a8ccac4
JorgeGomez <jgomez-1014@hotmail.com>
2022-05-10 22:43:48 -0300

    segundo commit

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,22 +1,114 @@
-console.log("Estamos en el archivo de la calculadora");
+console.log('Estamos en el archivo de la Calculadora');
 
-let numerouno = Number(prompt("Ingresa un numero para realizar en tu calculo: "))
-let numerodos = Number(prompt("Ingresa otro numero para realizar en tu calculo: "))
+/* let numeroUno = document.getElementById('numeroUno').value;
+let numeroDos = document.getElementById('numeroDos').value;
 
-console.log("El primer numero es: ", numerouno);
-console.log("El segundo numero es: ", numerodos);
+    console.log('EL primer número es: ', numeroUno);
+    console.log('El segundo número es: ', numeroDos);
 
+    function calcular()
+{
 
-function escribir(r){
-    console.log("El resultado es", r);
+    let resultado = Number(numeroUno) + Number(numeroDos);
+    console.log('El resultado es: ', resultado);
 }
+calcular(numeroUno,numeroDos) */
 
-function sumar(a,b){
+/*      
+function Sumar(a, b) {
     let resultado = a + b;
     escribir(resultado)
 }
-sumar(a,b);
 
-function restar(a,b){
+
+function Resta(a, b) {
     let resultado = a - b;
-}
+     */
+
+    var numeroUno, numeroDos, suma, resta, multi, dividir;
+
+    let resultado = document.getElementById('resultado');
+    let resultado2 = document.getElementById('resultado2');
+    let resultado3 = document.getElementById('resultado3');
+    let resultado4 = document.getElementById('resultado4');
+    
+    function calcular() {
+        
+        numeroUno = document.getElementById('numeroUno').value;
+        numeroDos = document.getElementById('numeroDos').value;
+
+        numeroUno = Number(numeroUno);
+        numeroDos = Number(numeroDos);
+
+        suma = numeroUno + numeroDos;
+        console.log("La suma de los dos valores es: ", suma);
+        resultado.innerHTML = "la suma de los dos valores es: " + suma;
+
+        resta = numeroUno - numeroDos;
+        console.log("La restaa de los dos valores es: ", resta);
+        resultado2.innerHTML = "la resta de los dos valores es: " + resta;
+
+        
+        multi = numeroUno * numeroDos;
+        console.log("La multiplicacion de los dos valores es: ", multi);
+        resultado3.innerHTML = "la Multiplicacion de los dos valores es: " + multi;
+
+        
+        dividir = numeroUno / numeroDos;
+        console.log("La divicion de los dos valores es: ", dividir);
+        resultado4.innerHTML = "la Divicion de los dos valores es: " + dividir;
+    }
+
+    function restar() {
+        
+        numeroUno = document.getElementById('numeroUno').value;
+        numeroDos = document.getElementById('numeroDos').value;
+
+        numeroUno = Number(numeroUno);
+        numeroDos = Number(numeroDos);
+
+        resta = numeroUno - numeroDos;
+        console.log("La resta de los dos valores es: ", resta);
+        resultado2.innerHTML = "la resta de los dos valores es: " + resta;
+    }
+
+    function multi() {
+        
+        numeroUno = document.getElementById('numeroUno').value;
+        numeroDos = document.getElementById('numeroDos').value;
+
+        numeroUno = Number(numeroUno);
+        numeroDos = Number(numeroDos);
+
+        
+        multi = numeroUno * numeroDos;
+        console.log("La multiplicacion de los dos valores es: ", multi);
+        resultado3.innerHTML = "la Multiplicacion de los dos valores es: " + multi;
+
+    }
+
+    function dividir() {
+        
+        numeroUno = document.getElementById('numeroUno').value;
+        numeroDos = document.getElementById('numeroDos').value;
+
+        numeroUno = Number(numeroUno);
+        numeroDos = Number(numeroDos);
+        
+        dividir = numeroUno / numeroDos;
+        console.log("La divicion de los dos valores es: ", dividir);
+        resultado4.innerHTML = "la Division de los dos valores es: " + dividir;
+    }
+
+    function sumar() {
+        
+        numeroUno = document.getElementById('numeroUno').value;
+        numeroDos = document.getElementById('numeroDos').value;
+
+        numeroUno = Number(numeroUno);
+        numeroDos = Number(numeroDos);
+
+        resta = numeroUno + numeroDos;
+        console.log("La Suma de los dos valores es: ", resta);
+        resultado2.innerHTML = "la suma de los dos valores es: " + resta;
+    }
